a9181ef2a5b0b1b5387a439e21d98da7dd0378f9
Nick Conlon <ncon67@gmail.com>
2016-01-30 18:50:39 -0500

    Added basic drag and drop functionality

--- Joestown/Assets/Scripts/MainManagerScript.cs
+++ Joestown/Assets/Scripts/MainManagerScript.cs
@@ -5,11 +5,25 @@ public class MainManagerScript : MonoBehaviour
 {
 	public Transform hightlight;
 
+	public LayerMask defaultRaycastLayerMask;
+	public LayerMask roomRaycastLayerMask;
+
 	private MemberScript m_selectedMember;
 
 	void Update ()
 	{
-		if( Input.GetMouseButtonDown( 0 ) )
+		if( m_selectedMember != null )
+		{
+			if( Input.GetMouseButton( 0 ) )
+			{
+				HandleDrag();
+			}
+			else if( Input.GetMouseButtonUp( 0 ) )
+			{
+				HandleRelease();
+			}
+		}
+		else if( Input.GetMouseButtonDown( 0 ) )
 		{
 			CheckClick();
 		}
@@ -17,41 +31,70 @@ public class MainManagerScript : MonoBehaviour
 
 	private void CheckClick()
 	{
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-		RaycastHit2D hit = Physics2D.Raycast( mousePos, Vector3.forward );
+		GameObject clickedObj = CheckMouseOver( defaultRaycastLayerMask );
 
-		if( hit.collider == null )
+		if( clickedObj == null )
 		{
-			if( m_selectedMember != null )
-			{
-				DeselectMember();
-			}
-
-			// early return
+			// Early return
 			return;
 		}
 
-		if( hit.collider.tag == "Member" )
+		if( clickedObj.tag == "Member" )
 		{
-			SelectMember( hit.collider.gameObject.GetComponent<MemberScript>() );
+			SelectMember( clickedObj.GetComponent<MemberScript>() );
             // open member ui
 		}
-		else if( hit.collider.tag == "Room" )
+		else if( clickedObj.tag == "Room" )
 		{
-			if( m_selectedMember != null )
+			/*if( m_selectedMember != null )
 			{
 				AssignSelectedMember( hit.collider.gameObject.GetComponent<RoomScript>() );
 			}
 			else
 			{
 				// Select room
-			}
+			}*/
+		}
+	}
+
+	private GameObject CheckMouseOver( LayerMask mask )
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+		RaycastHit2D hit = Physics2D.Raycast( mousePos, Vector3.forward, 20, mask );
+
+		if( hit.collider == null )
+		{
+			// early return
+			return null;
+		}
+
+		return hit.collider.gameObject;
+	}
+
+	private void HandleDrag()
+	{
+		m_selectedMember.transform.position = Camera.main.ScreenToWorldPoint( Input.mousePosition ) + new Vector3( 0.0f, 0.0f, 10.0f );
+	}
+
+	private void HandleRelease()
+	{
+		GameObject overObj = CheckMouseOver( roomRaycastLayerMask );
+
+		if( overObj == null )
+		{
+			return;
+		}
+
+		if( overObj.tag == "Room" )
+		{
+			AssignSelectedMember( overObj.GetComponent<RoomScript>() );
 		}
 	}
 
 	private void SelectMember( MemberScript selectedMember )
 	{
 		m_selectedMember = selectedMember;
+		m_selectedMember.GetComponent<Collider2D>().enabled = false;
 
 		hightlight.gameObject.SetActive( true );
 		hightlight.position = selectedMember.transform.position;
@@ -60,6 +103,7 @@ public class MainManagerScript : MonoBehaviour
 
 	private void DeselectMember()
 	{
+		m_selectedMember.GetComponent<Collider2D>().enabled = true;
 		m_selectedMember = null;
 
 		hightlight.parent = null;
