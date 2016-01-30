11345fcf5ec6b3f67c11001c10dcfa6eff114d6f
Nick Conlon <ncon67@gmail.com>
2016-01-29 22:50:14 -0500

    Added basic character, room, and manager functionality

---
+++ Joestown/Assets/Scripts/RoomScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomScript : MonoBehaviour
+{
+	public int maxMembers = 4;
+
+	public List<MemberScript> m_assignedMembers;
+
+	void Start()
+	{
+		m_assignedMembers = new List<MemberScript>();
+	}
+
+	public void AssignMember( MemberScript memberToAssign )
+	{
+		if( m_assignedMembers.Count < maxMembers )
+		{
+			m_assignedMembers.Add( memberToAssign );
+		}
+	}
+
+	public void RemoveMember( MemberScript memberToRemove )
+	{
+		m_assignedMembers.Remove( memberToRemove );
+	}
+
+	public bool CanAssignMember()
+	{
+		return m_assignedMembers.Count < maxMembers;
+	}
+}
