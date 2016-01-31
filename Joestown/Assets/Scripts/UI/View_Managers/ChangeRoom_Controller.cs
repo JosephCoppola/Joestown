0f2d938f59521ad778356116371d12d8ca7640a8
JosephCoppola <jjc9718@rit.edu>
2016-01-31 10:54:46 -0500

    Added descriptions to rooms

--- Joestown/Assets/Scripts/UI/View_Managers/ChangeRoom_Controller.cs
+++ Joestown/Assets/Scripts/UI/View_Managers/ChangeRoom_Controller.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ChangeRoom_Controller : MonoBehaviour {
@@ -9,17 +10,43 @@ public class ChangeRoom_Controller : MonoBehaviour {
 	{
 		get{ return display_Controller;}
 	}
+	[SerializeField]
+	private Button defaultButton;
+	[SerializeField]
+	private Button worshipButton;
+	[SerializeField]
+	private Button housingButton;
 
 	private RoomScript selectedRoom;
 
 	public void SelectRoom(RoomScript room)
 	{
 		selectedRoom = room;
+
+		if(room.roomType == RoomScript.RoomType.DEFAULT)
+		{
+			defaultButton.interactable = false;
+		}
+		else if(room.roomType == RoomScript.RoomType.WORSHIP)
+		{
+			worshipButton.interactable = false;
+		}
+		else if(room.roomType == RoomScript.RoomType.HOUSING)
+		{
+			housingButton.interactable = false;
+		}
 	}
 
 	public void ChangeRoom(int selection)
 	{
 		selectedRoom.roomType = (RoomScript.RoomType)selection;
 		display_Controller.SetScreenView(false);
+	}
+
+	void OnDisable()
+	{
+		defaultButton.interactable = true;
+		worshipButton.interactable = true;
+		housingButton.interactable = true;
 	}
 }
