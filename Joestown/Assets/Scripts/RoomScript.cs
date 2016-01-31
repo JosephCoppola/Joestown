83e991f6aac52228ebf495445cdc55dca9ece166
Nick Conlon <ncon67@gmail.com>
2016-01-31 10:04:46 -0500

    Added text popup when recruiter returns

--- Joestown/Assets/Scripts/RoomScript.cs
+++ Joestown/Assets/Scripts/RoomScript.cs
@@ -26,10 +26,7 @@ public class RoomScript : MonoBehaviour
 		get { return m_assignedMembers; }
 	}
 
-	private Transform[] memberSlots;
-
-	//private float m_currTime = 0.0f;
-	//private float m_updateInterval = 1.0f;
+	public Transform[] memberSlots;
 
     public RoomType Type
     {
@@ -46,7 +43,7 @@ public class RoomScript : MonoBehaviour
 
 		Transform slotContainer = transform.FindChild( "Member Slots" );
 
-		if( slotContainer != null && slotContainer.childCount == maxMembers )
+		if( slotContainer != null && slotContainer.childCount >= maxMembers )
 		{
 			for( int i = 0; i < maxMembers; i++ )
             {
@@ -55,27 +52,20 @@ public class RoomScript : MonoBehaviour
 		}
 		else
 		{
-			// If there are no slots, just space out the spawn locations
+			print( "Room doesn't have member slots :(" );
+			/*// If there are no slots, just space out the spawn locations
 			for( int i = 0; i < maxMembers; i++ )
 			{
 				memberSlots[ i ] = new GameObject( "Member Slots" ).transform;
 				memberSlots[ i ].parent = transform;
 				memberSlots[ i ].position = new Vector3( 1 / maxMembers * i - 0.5f, 0.125f, 0 );
-            }
+            }*/
         }
     }
     
     void Update()
 	{
 		UpdateStats();
-
-		/*if( m_currTime >= m_updateInterval )
-		{
-			UpdateStats();
-			m_currTime = 0;
-		}
-
-		m_currTime += Time.deltaTime;*/
 	}
 
 	public void AssignMember( MemberScript memberToAssign )
@@ -99,7 +89,6 @@ public class RoomScript : MonoBehaviour
 	public void RemoveMember( MemberScript memberToRemove )
 	{
 		m_assignedMembers.Remove( memberToRemove );
-		//memberToRemove.transform.parent = null;
 	}
 
 	public bool CanAssignMember()
